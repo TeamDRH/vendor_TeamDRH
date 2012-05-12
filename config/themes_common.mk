3cd66dff595d76dd8851bcd2348d2ee2904d0e26
wjb <jaybob413@gmail.com>
2012-05-12 17:13:45 -0400

    add lunch menu items and cm9 perms
    Change-Id: Ic8b98ea5fb63720eae0aa8e509e032855fcb5b97

--- config/themes_common.mk
+++ config/themes_common.mk
@@ -5,6 +5,6 @@ PRODUCT_PACKAGES += \
        com.tmobile.themes
 
 PRODUCT_COPY_FILES += \
-       vendor/TeamDRH/config/permissions/com.tmobile.software.themes.xml:system/etc/permissions/com.tmobile.software.themes.xml
+       vendor/TeamDRH/config/permissions/com.tmobile.software.themes.xml:system/etc/permissions/com.tmobile.software.themes.xml \
        vendor/TeamDRH/config/permissions/com.cyanogenmod.android.xml:system/etc/permissions/com.cyanogenmod.android.xml
 
--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,3 +1,4 @@
-#add_lunch_combo TeamDRH_smba1002-eng
+add_lunch_combo TeamDRH_smba1002-eng
+add_lunch_combo TeamDRH_smba1002-userdebug
 
 PATH=$PATH:$PWD/vendor/TeamDRH/tools ; export PATH
